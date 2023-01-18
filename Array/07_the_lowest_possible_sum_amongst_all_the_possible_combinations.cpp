38c45308cfd45a882de17341876af3a8e33050ea
cmrohityadav <cmrohityadav23@gmail.com>
2023-01-18 19:24:04 +0530

    Given two arrays a[] and b[] of same size.Your task is to find the minimum sum of two elements such that they belong to different arrays and are not at the same index. Here 1<size<101 For ex: a[]={5,6,10,4,9} b[]={1,2,3,4,5} Output: 5 Explanation: a[3] + b[0] = 5 is the lowest possible sum amongst all the possible combinations a[3] + b[0] = 5 is the lowest possible sum amongst all the possible combinations

---
+++ Array/07_the_lowest_possible_sum_amongst_all_the_possible_combinations.cpp
@@ -0,0 +1,15 @@
+/*
+- Given two arrays a[] and b[] of same size.Your task is to find the minimum sum of two elements such
+that they belong to different arrays and are not at the same index. Here 1<size<101
+For ex: a[]={5,6,10,4,9}
+b[]={1,2,3,4,5}
+Output: 5
+Explanation: a[3] + b[0] = 5 is the lowest possible sum amongst all the possible combinations
+a[3] + b[0] = 5 is the lowest possible sum amongst all the possible combinations
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int  main(){
+cout<<"hello";
+    return 0; 
+}
