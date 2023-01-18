3969895ba2da949a35f664c7956f9c5d285dd949
cmrohityadav <cmrohityadav23@gmail.com>
2023-01-18 17:59:08 +0530

    linear search

---
+++ Array/03_linearSearch.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+ 
+cout<<"enter the key value"<<endl;
+int key;
+int ans=-1;
+cin>>key;
+int arr[5]={1,2,3,4,5};
+for(int i=0;i<5;i++){
+if(arr[i]==key){
+    ans=i;
+    break;
+}
+
+}
+
+cout<<ans<<endl;
+
+    return 0;
+ }
