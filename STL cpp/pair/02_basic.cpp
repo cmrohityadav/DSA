0540bb025e76c45a6fe929ba2fd481fe2f4067b9
cmrohityadav <cmrohityadav23@gmail.com>
2023-01-19 14:37:54 +0530

    pair stl

---
+++ STL cpp/pair/02_basic.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    pair<int,int>p(10,20);
+    cout<<p.first<<endl;
+    cout<<p.second<<endl;
+    pair<int,string>r(22,"Rohit");
+    cout<<r.first<<" "<<r.second<<endl;
+
+
+    //another to declare
+
+    pair<int, int>num;
+    num={12,125};
+    cout<<num.first<<endl;
+
+    //here just initliaze the pair doest declare
+ cout<<"test_1"<<endl;
+    pair<int, int>f;
+    cout<<f.first<<endl;
+    cout<<f.second<<endl;
+
+
+cout<<"test2"<<endl;
+    pair<int,char>ch;
+    cout<<ch.second<<endl;
+    cout<<ch.first<<endl;
+
+    
+cout<<"test 3"<<endl;
+pair<int,string>str;
+    cout<<ch.second<<endl;
+    cout<<ch.first<<endl;
+
+}
---
+++ STL cpp/pair/03_comparisionOfPair.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,int>p1(12,14),p2(12,13);
+    cout<<"now p1 "<<p1.first<<" "<<p1.second<<" p2 now "<<p2.first<<" "<<p2.second<<endl;
+    cout<<endl;
+
+//compare: /*isme first pair value agar sahi rahi to result show krta hai  
+/*agar first pair value wrong hai to next pair se compare krta hai */
+
+
+
+    // cout<<(p1==p2)<<endl; //dono k first item ko copare krta hai sahi hua to result show krega warna fir second pair value ko compare karega
+    // cout<<(p1!=p2)<<endl;
+    // cout<<(p1>p2)<<endl;
+    // cout<<(p1<p2)<<endl;
+    // cout<<(p1==p2)<<endl;
+
+    return 0;
+}
